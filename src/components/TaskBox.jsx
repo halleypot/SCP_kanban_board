d22f0ee4a75de736690a5ea5c10599e8bdc2d6dd
halleypot <halleypot@163.com>
2023-02-26 12:07:14 -0400

    finish drag-and-drop function

--- src/components/TaskBox.jsx
+++ src/components/TaskBox.jsx
@@ -1,7 +1,85 @@
-const TaskBox = () => {
+import React, { useCallback } from 'react'
+import Column from './Column'
+import { DragDropContext } from 'react-beautiful-dnd'
+
+const TaskBox = ({ events, setEvents, currentEvent, setCurrentEvent }) => {
+  const handleRemove = useCallback(() => {
+    if (window.confirm('You really want to remove it?')) {
+      // update events
+      setEvents(prev => {
+        console.log(prev)
+        const result = prev.filter(item => item.title !== currentEvent.title)
+        // if event is empty
+        if (!result.length) {
+          // init the event
+          const initEvent = [
+            {
+              title: 'New Event',
+              ['To do']: [],
+              ['In progress']: [],
+              ['Completed']: []
+            }
+          ]
+          setEvents(initEvent)
+        } else {
+          // set the first event as current
+          setCurrentEvent(result[0])
+        }
+        return result
+      })
+    }
+  }, [events, setEvents, currentEvent, setCurrentEvent])
+
+  const handleDragEnd = useCallback(
+    result => {
+      console.log(typeof result)
+      if (!result.destination) return
+      const { source, destination } = result
+      const curEvent = events.find(item => item.title === currentEvent.title)
+      const taskCopy = curEvent[source.droppableId][source.index]
+      setEvents(prev =>
+        prev.map(event => {
+          if (event.title === currentEvent.title) {
+            let eventCopy = { ...event }
+            // Remove from source
+            const taskListSource = event[source.droppableId]
+            taskListSource.splice(source.index, 1)
+            eventCopy = { ...event, [source.droppableId]: taskListSource }
+            // Add to destination
+            const taskListDes = event[destination.droppableId]
+            taskListDes.splice(destination.index, 0, taskCopy)
+            eventCopy = { ...event, [destination.droppableId]: taskListDes }
+            return eventCopy
+          } else {
+            return event
+          }
+        })
+      )
+    },
+    [events, setEvents, currentEvent]
+  )
+
   return (
-    <div>
-      <h1>TaskBox</h1>
+    <div className='task-box'>
+      <header className='task-box-header'>
+        <h1 className='task-box-title'>Todo List</h1>
+        <button className='remove-button' onClick={handleRemove}>
+          Remove this Project
+        </button>
+      </header>
+      <DragDropContext onDragEnd={result => handleDragEnd(result)}>
+        <div className='task-box-body'>
+          {['To do', 'In progress', 'Completed'].map(tag => (
+            <Column
+              key={tag}
+              tag={tag}
+              events={events}
+              setEvents={setEvents}
+              currentEvent={currentEvent}
+            />
+          ))}
+        </div>
+      </DragDropContext>
     </div>
   )
 }
